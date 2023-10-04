12f20759cdc2af50c6db91463338d43099f8be54
Renat Kovshikov <renat.kovshikov@gmail.com>
2023-10-04 16:52:05 +0300

    add modal window

---
+++ scripts/app.js
@@ -0,0 +1,25 @@
+// Select movie poster original image
+let moviePoster = document.querySelector('.movie-poster');
+// Select modal window layer
+let modal = document.querySelector('#myModal');
+// Select close button
+let closeBtn = document.querySelector('.closeBtn');
+// Select modal window image
+let modalImage = document.querySelector('.movie-poster-modal');
+
+moviePoster.addEventListener("click", () => {
+    console.log("image clicked");
+    modal.style.display = "block";
+    modalImage.src = moviePoster.src;
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if(event.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
